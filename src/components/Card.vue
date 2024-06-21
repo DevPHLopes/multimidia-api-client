9d4c7254d8af7dfdfae70e6fff075ded8d03d782
Philipe Lopes <silva.lopes.philipe@gmail.com>
2024-06-21 20:17:37 -0300

    feat: cria card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,15 +1,12 @@
 <template>
     <div class="card">
       <p class="card-title"><img :src="time.team.logos[0].href" height="100px"></p>
-      <p class="card-title">{{ times.nome }}</p>
+      <p class="card-title">{{ time.team.name }}</p>
       <ul class="card-description">
-        <li>{{ times.idade }}</li>
-        <li>{{ times.estaduais}}</li>
-        <li>{{ times.brasileiros}}</li>
-        <li>{{ times.libertadores}}</li>
-        <li>{{ times.copabrasil}}</li>
-        <li>{{ times.sulamericana}}</li>
-        <li>{{ times.mundiais}}</li>
+      <li>{{ time.stats[10].value}}° Lugar</li>
+      <li>{{ time.stats[3].value}} Pontos</li>
+      <li>{{ time.stats[7].value}} Vitorias</li>
+      <li>{{ time.stats[5].value}} Gols</li>
       </ul>
     </div>
 </template>
@@ -18,8 +15,10 @@
 export default {
     name: 'Card',
     props: {
-        type: Object,
-        required: true
+      time: {
+          type: Object,
+          required: true
+      }
     }
 }
 </script>
